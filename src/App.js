88a674ecf95821d52e1d3cdc70a86faeeb1ffee9
Sivam Patel <patel.sivam@gmail.com>
2021-08-12 09:08:04 -0500

    Deployment and small bug fixes

--- src/App.js
+++ src/App.js
@@ -10,15 +10,15 @@ const areas = ['state', 'county', 'cbsa', 'country']
 function App() {
   return (
     <div className="App">
-      <HashRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter>
         <Switch>
           <Route path={"/:area/:id"} render={(props) => areas.includes(props.match.params.area) ? <View {...props} /> : <Redirect to="/" /> } />
-          <Route from="/bayarea" to="/cbsa/41860" />
-          <Route from="/dfw" to="/cbsa/19100" />
-          <Route from="/sanjose" to="/cbsa/41940" />
-          <Route from="/dallas" to="/county/01047" />
-          <Route from="/sf" to="/county/06075" />
-          <Route from="/sanmateo" to="/county/06081" />
+          <Redirect from="/bayarea" to="/cbsa/41860" />
+          <Redirect from="/dfw" to="/cbsa/19100" />
+          <Redirect from="/sanjose" to="/cbsa/41940" />
+          <Redirect from="/dallas" to="/county/01047" />
+          <Redirect from="/sf" to="/county/06075" />
+          <Redirect from="/sanmateo" to="/county/06081" />
           <Route render={() => <Redirect to="/country/us" />} />
         </Switch>
       </HashRouter>
